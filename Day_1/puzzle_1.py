3aec0545f1bc3c76b040054014f20107f4841351
JuanGonzalezCaminero <juangonzalezcaminero@protonmail.com>
2021-12-01 11:58:10 +0100

    Day 1

--- Day_1/puzzle_1.py
+++ Day_1/puzzle_1.py
@@ -1,1 +1,7 @@
-
+file = open("input")
+data = [int(l) for l in file.readlines()];
+result = 0
+for i in range(1, len(data)):
+	if data[i]>data[i-1]:
+		result+=1
+print(result)
--- Day_1/puzzle_2.py
+++ Day_1/puzzle_2.py
@@ -1,1 +1,8 @@
+file = open("input")
+data = [int(l) for l in file.readlines()];
+result = 0
+for i in range(3, len(data)):
+	if sum(data[i-2:i+1])>sum(data[i-3:i]):
+		result+=1
+print(result)
 
