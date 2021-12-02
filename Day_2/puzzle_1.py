95ef864c8497ffac1f0cc491eaf657f51b75fc4f
JuanGonzalezCaminero <juangonzalezcaminero@protonmail.com>
2021-12-02 12:42:46 +0100

    Day 2

--- Day_2/puzzle_1.py
+++ Day_2/puzzle_1.py
@@ -1,1 +1,14 @@
+file = open("input")
+commands = [[i[0], int(i[1])] for i in [j.strip().split(" ") for j in file.readlines()]]
+#[horizontal, vertical]
+position=[0,0]
 
+for c in commands:
+	if c[0]=="forward":
+		position[0]+=c[1]
+	elif c[0]=="down":
+		position[1]+=c[1]
+	elif c[0]=="up":
+		position[1]-=c[1]
+
+print(position[0]*position[1])
