29b3945791c8c1f65b401cf14d964514b2053f29
JuanGonzalezCaminero <juangonzalezcaminero@protonmail.com>
2021-12-06 11:37:32 +0100

    Day 6

--- Day_6/puzzle_1.py
+++ Day_6/puzzle_1.py
@@ -1,1 +1,14 @@
 
+file = open("input")
+
+data = [int(i) for i in file.read().split(",")]
+fish = [data.count(i) for i in range(9)]
+
+for day in range(80):
+	new_fish=fish[0]
+	fish[:7]=fish[1:7]+[fish[0]]
+	fish[6]+=fish[7]
+	fish[7]=fish[8]
+	fish[8]=new_fish
+
+print(sum(fish))
